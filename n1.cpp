f23c32dd204eecc8424417c3e2c8bb0add7df082
BorisenkoSergey <61838037+BorisenkoSergey@users.noreply.github.com>
2020-05-12 15:34:24 +0300

    3
    Я запустил цикл do while, для того чтобы заполнять массив числами, которые вводит пользователь, пока n != -9999

--- n1.cpp
+++ n1.cpp
@@ -5,4 +5,16 @@ int main()
   int array1[1000];
   int count = 0;
   int n;
+  do
+  {
+    cout << "Введите число ";
+    cin >> n;
+    if (n != -9999)
+    {
+      array1[count] = n;
+      count++;
+    }
+  }
+  while(n != -9999);
+    
 }
